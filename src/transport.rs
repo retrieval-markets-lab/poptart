f801a59b2badacc3c98da9b869bcf349746f8257
Alexander Camuto <alexander.camuto@st-hughs.ox.ac.uk>
2022-12-07 15:31:19 +0000

    cleanup

--- src/transport.rs
+++ src/transport.rs
@@ -15,29 +15,26 @@ pub async fn build_transport(
     keys: &Keypair,
     is_relay_client: bool,
 ) -> (Boxed<(PeerId, StreamMuxerBox)>, Option<RelayClient>) {
-    // TCP
     let tcp_config = tcp::Config::default().port_reuse(true);
     let tcp_transport = tcp::tokio::Transport::new(tcp_config.clone());
-    // Websockets
+
     let ws_tcp = websocket::WsConfig::new(tcp::tokio::Transport::new(tcp_config));
     let tcp_ws_transport = tcp_transport.or_transport(ws_tcp);
-    // Quic
+
     let quic_config = quic::Config::new(&keys);
     let quic_transport = quic::tokio::Transport::new(quic_config);
-    // Noise config for TCP & Websockets
+
     let auth_config = {
         let dh_keys = noise::Keypair::<noise::X25519Spec>::new()
             .into_authentic(&keys)
             .expect("Noise key generation failed");
         noise::NoiseConfig::xx(dh_keys).into_authenticated()
     };
-    // Stream muxer config for TCP & Websockets
+
     let mut yamux_config = yamux::YamuxConfig::default();
     yamux_config.set_max_buffer_size(16 * 1024 * 1024);
     yamux_config.set_receive_window_size(16 * 1024 * 1024);
     yamux_config.set_window_update_mode(WindowUpdateMode::on_receive());
-
-    // Websockets
 
     let (tcp_ws_transport, relay_client) = if is_relay_client {
         let (relay_transport, relay_client) =
@@ -67,8 +64,6 @@ pub async fn build_transport(
             EitherOutput::Second((peer_id, muxer)) => (peer_id, StreamMuxerBox::new(muxer)),
         })
         .boxed();
-
-    // DNS
 
     let dns_cfg = dns::ResolverConfig::cloudflare();
     let dns_opts = dns::ResolverOpts::default();
